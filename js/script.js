95374eb2524824ce8de179b94d8c65c7c4de75d7
Grzegorz Rzeznikiewicz <grzegorzrzeznikiewicz@gmail.com>
2025-12-27 22:28:14 +0100

    Add preventDefault to title click handler to prevent default link behavior

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     /* [DONE] remove class 'active' from all article links */
