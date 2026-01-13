f4ca54ee157fc25378b5a6870722a04f6d85b0c6
Grzegorz Rzeznikiewicz <grzegorzrzeznikiewicz@gmail.com>
2026-01-13 21:33:37 +0100

    Remove TypeScript ESLint configuration and dependencies

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 {
     const titleClickHandler = function (event) {
         event.preventDefault();
-        // eslint-disable-next-line @typescript-eslint/no-this-alias
         const clickedElement = this;
         console.log('Link was clicked!');
         /* [DONE] remove class 'active' from all article links */
